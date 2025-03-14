cda8f69e5af0c16a0e343246853293514990074a
Hale <Peter.Hale@edfenergy.com>
2025-03-14 10:51:10 +0000

    added add_missing_col() function

---
+++ R/add_missing_column.R
@@ -0,0 +1,44 @@
+#' Add Missing Column
+#'
+#' @description
+#' Add a missing column to a data frame
+#'
+#'
+#' @param d data frame
+#' @param col column name to be added
+#' @param type column type ('character','integer' or 'numeric', else defaults to 'logical')
+#'
+#' @return tibble
+#' @export
+#'
+#' @examples
+#' # Add a new character column
+#' tibble::tibble(a=1, b=2) |> add_missing_col(col='c',type='character')
+#'
+#' Trying to add a column that already exists returns the existing data frame
+#' tibble::tibble(a=1, b=2) |> add_missing_col(col='a',type='character')
+#'
+add_missing_col <- function(d, col, type) {
+
+  if({{col}} %in% names(d)) {
+    return(d)
+  }
+
+  if(type=='character') {
+    d <- dplyr::mutate(d, {{col}} := NA_character_)
+  } else if(type=='integer' | type=='int') {
+    d <- dplyr::mutate(d, {{col}} := NA_integer_)
+  } else if(type=='numeric') {
+    d <- dplyr::mutate(d, {{col}} := NA_real_)
+  } else {
+    d <- dplyr::mutate(d, {{col}} := NA)
+  }
+
+  d
+}
+
+
+
+
+
+
