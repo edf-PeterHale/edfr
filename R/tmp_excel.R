dbf098f54ac15c40c649655946bf17eb7a6b4a68
Hale <Peter.Hale@edfenergy.com>
2025-04-07 09:39:49 +0100

    added tmp_excel() function

---
+++ R/tmp_excel.R
@@ -0,0 +1,16 @@
+#' Write Temp XLSX File
+#'
+#' @description
+#' Write data to a temporary .xlsx file and open the file. Useful when you need to share a data set with others.
+#'
+#'
+#' @param df Dataframe to be opened in Excel
+#'
+#' @return Excel file
+#' @export
+#'
+tmp_excel <- function(df) {
+  f <- paste0(tempfile(),'.xlsx')
+  writexl::write_xlsx(df, f)
+  shell.exec(f)
+}
