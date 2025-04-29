85a81e9d38daacea5df65dd528bdb174da2a7a20
Hale <Peter.Hale@edfenergy.com>
2025-04-29 09:23:14 +0100

    added get_mop_portfolio() function

---
+++ R/get_mop_portfolio.R
@@ -0,0 +1,33 @@
+#' Get MOP Portfolio
+#'
+#' @description
+#' Query the live MOP portfolio
+#'
+#'
+#' @param supp Include supplementary fields, Default is FALSE
+#'
+#' @return tibble
+#' @export
+#'
+get_mop_portfolio <- function(supp=FALSE) {
+
+  if(supp) {
+    q_ <- "select
+           mp.*,
+           sup.* exclude(RECORD_ID, MPAN, METER_TYPE, MOP_START, MOP_END, SUPPLIER_TIER, SUPPLIER, MTS_METER_TYPE, SERIAL_NO)
+          from WORKINGDB_ALTERYX_EBS_DEV.DEV_CORE_MO.LIVE_MOP_PORTFOLIO mp
+          left join WORKINGDB_ALTERYX_EBS_DEV.DEV_CORE_MO.MOP_PORTFOLIO_SUPPLEMENTARY sup using(record_id)"
+  } else {
+    q_ <- "select * from WORKINGDB_ALTERYX_EBS_DEV.DEV_CORE_MO.LIVE_MOP_PORTFOLIO"
+  }
+
+  edfr::query_sf(q_)
+}
+
+
+
+
+
+
+
+
