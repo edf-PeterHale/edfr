9aac07e1706293de9f20dc87fa55890cc7d2f9f8
Hale <Peter.Hale@edfenergy.com>
2025-06-13 12:59:10 +0100

    added tracing argument to query_c2m()

--- R/query_c2m.R
+++ R/query_c2m.R
@@ -12,10 +12,10 @@
 #' @export
 #'
 query_c2m <- function(q, dsn = 'C2M', database = 'CISADM', bigint = 'integer',
-                      LogLevel = 0, params = list(), show.query = FALSE) {
+                      LogLevel = 0, tracing=1, params = list(), show.query = FALSE) {
 
   conn <- DBI::dbConnect(drv = odbc::odbc(), dsn = dsn, database = database, bigint = bigint,
-                         LogLevel = 0, PWD = Sys.getenv('C2MPWD'))
+                         LogLevel = 0, tracing=tracing, PWD = Sys.getenv('C2MPWD'))
 
   iq <- DBI::sqlInterpolate(conn, q, .dots = params)
   if(show.query) {message(iq)}
