d21d8ede9df98d0c4ab30a51c89255fac8693a55
Hale <Peter.Hale@edfenergy.com>
2025-06-17 14:43:33 +0100

    added cols argument to get_mop_portfolio. Allows column selection within query

--- R/get_mop_portfolio.R
+++ R/get_mop_portfolio.R
@@ -3,22 +3,28 @@
 #' @description
 #' Query the live MOP portfolio
 #'
-#'
+#' @param cols Vector of columns to select. Default to all columns
 #' @param supp Include supplementary fields, Default is FALSE
 #'
 #' @return tibble
 #' @export
 #'
-get_mop_portfolio <- function(supp=FALSE) {
+get_mop_portfolio <- function(cols = '*', supp=FALSE) {
 
   if(supp) {
-    q_ <- "select
-           mp.*,
-           sup.* exclude(RECORD_ID, MPAN, METER_TYPE, MOP_START, MOP_END, SUPPLIER_TIER, SUPPLIER, MTS_METER_TYPE, SERIAL_NO, DATA_REFRESHED)
-          from WORKINGDB_ALTERYX_EBS_PRD.PRD_CORE_MO.IFS_MOP_PORTFOLIO mp
-          left join WORKINGDB_ALTERYX_EBS_PRD.PRD_CORE_MO.IFS_MOP_PORTFOLIO_SUPPLEMENTARY sup using(record_id)"
+    q_ <- stringr::str_glue("
+    with a as (
+    select
+    mp.*,
+    sup.* exclude(RECORD_ID, MPAN, METER_TYPE, MOP_START, MOP_END, SUPPLIER_TIER, SUPPLIER, MTS_METER_TYPE, SERIAL_NO, DATA_REFRESHED)
+    from WORKINGDB_ALTERYX_EBS_PRD.PRD_CORE_MO.IFS_MOP_PORTFOLIO mp
+    left join WORKINGDB_ALTERYX_EBS_PRD.PRD_CORE_MO.IFS_MOP_PORTFOLIO_SUPPLEMENTARY sup using(record_id)
+    )
+    select {paste0(cols,collapse=',')} from a")
   } else {
-    q_ <- "select * from WORKINGDB_ALTERYX_EBS_PRD.PRD_CORE_MO.IFS_MOP_PORTFOLIO"
+    q_ <- stringr::str_glue("
+    select {paste0(cols,collapse=',')}
+    from WORKINGDB_ALTERYX_EBS_PRD.PRD_CORE_MO.IFS_MOP_PORTFOLIO")
   }
 
   edfr::query_sf(q_)
